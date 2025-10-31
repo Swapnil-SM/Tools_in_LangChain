44ec81dd39d169d178d435881e78d99f95cc9186
Swapnil-SM <swapnilmishra413@gmail.com>
2025-10-31 19:31:08 +0530

    toolkit

---
+++ toolkit.py
@@ -0,0 +1,22 @@
+from langchain_core.tools import tool
+
+# Custom tools
+@tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+class MathsToolkit:
+    def get_tool(self):
+        return [add, multiply]
+
+toolkit = MathsToolkit()
+tools = toolkit.get_tool()
+
+for tool in tools:
+    print(tool.name, "=>", tool.description)
